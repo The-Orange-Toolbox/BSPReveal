8b41feebec37b78fc0b3312a1d8941e779b9f706
maxdup <mdupuis@hotmail.ca>
2021-06-29 02:06:37 -0400

    improved log output

--- drawbbbrushes/__main__.py
+++ drawbbbrushes/__main__.py
@@ -1,7 +1,9 @@
 import argparse
 import traceback
+from datetime import datetime
 
 from clipify import clipify
+from _constants import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -13,13 +15,20 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', metavar='path', type=ascii, default='',
                         help='Where to save the modified bsp (overwrites the original by default)')
 
+    print('Community Tool - {name}.exe ({date})\n'.format(name=NAME,
+                                                          date=BUILD_DATE))
+
     args = parser.parse_args()
     in_bsp = eval(args.input)
     out_bsp = eval(args.output) or in_bsp
 
     try:
+        initial_time = datetime.now()
         clipify(in_bsp, out_bsp)
-        print('completed')
+
+        elapsed_time = datetime.now() - initial_time
+        elapsed_secs = elapsed_time.total_seconds()
+        print('{:.1f} seconds elapsed'.format(elapsed_secs))
 
     except Exception as e:
         print('something went wrong')
--- drawbbbrushes/clipify.py
+++ drawbbbrushes/clipify.py
@@ -1,9 +1,16 @@
+import os
+
 from valvebsp import *
 from valvebsp.lumps import *
 
 
 def clipify(in_bsp, out_bsp):
+
+    print('Loading {}'.format(os.path.abspath(in_bsp)))
     bsp = Bsp(in_bsp)
+    bb_count = 0
+
+    print('{} brushes'.format(len(bsp[LUMP_BRUSHES])))
 
     for brush in bsp[LUMP_BRUSHES]:
         brush_side = bsp[LUMP_BRUSHSIDES][brush.firstSide]
@@ -13,5 +20,9 @@ def clipify(in_bsp, out_bsp):
 
         if texname.startswith('TOOLS/TOOLSBLOCKBULLETS'):
             brush.contents.CONTENTS_MONSTERCLIP = True
+            bb_count += 1
 
+    print('{} blockbullet brushes modified'.format(bb_count))
+
+    print('Writing {}'.format(os.path.abspath(out_bsp)))
     bsp.save(out_bsp)
