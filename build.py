93176c215d7fe1b8bb83c372ff2c10535b8637d2
maxdup <mdupuis@hotmail.ca>
2021-06-29 02:06:28 -0400

    added build constants

--- build.py
+++ build.py
@@ -1,10 +1,21 @@
 import PyInstaller.__main__
+import datetime
+
+exename = 'drawbbbrushes'
+builddate = datetime.datetime.now().strftime('%b %d, %Y')
+version = "1.0"
+
+# Write version info into _constants.py resource file
+with open('drawbbbrushes/_constants.py', 'w') as f:
+    f.write("NAME = \"{}\"\n".format(exename))
+    f.write("VERSION = \"{}\"\n".format(version))
+    f.write("BUILD_DATE = \"{}\"\n".format(builddate))
 
 PyInstaller.__main__.run([
     'drawbbbrushes\__main__.py',
     '-F',
     '-p',
     'drawbbbrushes',
-    '-n',   
-    'drawbbbrushes',
+    '-n',
+    exename,
 ])
---
+++ drawbbbrushes/_constants.py
@@ -0,0 +1,3 @@
+NAME = "drawbbbrushes"
+VERSION = "1.0"
+BUILD_DATE = "Jun 29, 2021"
