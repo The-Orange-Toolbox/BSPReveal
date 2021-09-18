26ef3083265b53a2847a9649a635d640e199840c
maxdup <mdupuis@hotmail.ca>
2021-09-18 01:27:12 -0400

    improved update notifications

--- src/updater.py
+++ src/updater.py
@@ -5,19 +5,22 @@ from _constants import *
 
 def parse_version(vstr):
     vstrs = vstr.lstrip('v').split('.')
-    vs = [int(v) for v in vstrs]
-    return vs
+    return [int(v) for v in vstrs]
 
 
 def check_for_updates():
-    response = urllib.request.urlopen(
-        "https://api.github.com/repos/The-Orange-Toolbox/BSPReveal/releases/latest")
-    meta = json.loads(response.read())
-    version = parse_version(meta['tag_name'])
-    latest_version = parse_version(meta['tag_name'])
-    current_version = parse_version(VERSION)
+    try:
+        response = urllib.request.urlopen(
+            "https://api.github.com/repos/The-Orange-Toolbox/BSPReveal/releases/latest")
+        meta = json.loads(response.read())
+        latest_version = parse_version(meta['tag_name'])
+        current_version = parse_version(VERSION)
 
-    for i in range(len(current_version)):
-        if current_version[i] < latest_version[i]:
-            print("A new version of " + NAME + " is available")
-            print("https://github.com/The-Orange-Toolbox/BSPReveal/releases\n")
+        for i in range(len(current_version)):
+            if current_version[i] > latest_version[i]:
+                break
+            if current_version[i] < latest_version[i]:
+                print("A new version of " + NAME + " is available")
+                print("https://github.com/The-Orange-Toolbox/BSPReveal/releases\n")
+    except:
+        pass
